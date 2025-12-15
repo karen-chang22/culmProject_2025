ebc58526214b4a23fc21cea5cfa7ee020fb29cb5
karen.chang22 <chang.karen2008@gmail.com>
2025-12-15 10:44:08 -0500

    we initialized our templates folder

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Welcome to our Flask Web Application!"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
